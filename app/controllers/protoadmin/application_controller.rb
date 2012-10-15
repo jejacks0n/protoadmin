f033ac23d786fa0e0427bfa5db5396c697f2ac78
jejacks0n <jejacks0n@gmail.com>
2012-10-14 21:52:47 -0600

    provide additional data back to pjax via the X-PJAX-DATA response header.

--- app/controllers/protoadmin/application_controller.rb
+++ app/controllers/protoadmin/application_controller.rb
@@ -4,7 +4,7 @@ class Protoadmin::ApplicationController < ActionController::Base
 
   layout :layout
 
-  before_filter :authenticate_admin!, :add_base_crumbs, :add_crumbs
+  before_filter :authenticate_admin!, :add_base_crumbs, :add_crumbs, :create_body_class
 
   def dashboard
     render 'protoadmin/dashboard'
@@ -20,14 +20,24 @@ class Protoadmin::ApplicationController < ActionController::Base
 
   def add_base_crumbs
     return if devise_controller?
-    controller_name = self.class.name.gsub(/Controller$/, '')
-    unless controller_name == 'Protoadmin::Application'
-      link = (controller_name =~ /Protoadmin/ ? protoadmin : self).url_for(controller: params[:controller])
-      add_crumb controller_name.gsub(/Protoadmin::/, ''), link
+    if self.class.name !~ /^Protoadmin::ApplicationController/
+      link = (self.class.name =~ /^Protoadmin::/ ? protoadmin : self).url_for(controller: params[:controller])
+      add_crumb controller_name.titleize, link
     end
   end
 
   def add_crumbs
+  end
+
+  def set_pjax_url
+    super
+    response.headers['X-PJAX-DATA'] = {
+      body_class: "#{controller_name}_controller #{action_name}_action"
+    }.to_json
+  end
+
+  def create_body_class
+    @body_class = "#{controller_name}_controller #{action_name}_action"
   end
 
 end
